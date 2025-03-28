77f5f91d1e2e41c7e3dd81346c889d0ff07f98e5
Alex S <insten490@gmail.com>
2025-03-27 20:15:06 -0500

    updated main file

--- back-end/src/main.go
+++ back-end/src/main.go
@@ -1,9 +1,5 @@
 package main
 
-import "fmt"
-import "github.com/UkrainianProgrammer/PayUp-Go/back-end/src/testpackage"
-
 func main()  {
-	fmt.Println("Hello World")
-	testpackage.MyFunction()
+
 }
