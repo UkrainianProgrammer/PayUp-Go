57b7b464fa12b4e8356b38f11674d646ebcd42e9
Alex S <insten490@gmail.com>
2025-04-05 23:40:51 -0500

    added header for payment intent method

--- back-end/src/main.go
+++ back-end/src/main.go
@@ -6,8 +6,13 @@ import (
 )
 
 // http://localhost:4242/create-initial-payment
+// http://localhost:4042/health
 
 func main()  {
-	http.HandleFunc("/create-payment-intent")
+	http.HandleFunc("/create-payment-intent", handleCreatePaymentIntent)
 	http.HandleFunc("health")
+}
+
+func handleCreatePaymentIntent(w http.ResponseWriter, r *http.Request) {
+	
 }
