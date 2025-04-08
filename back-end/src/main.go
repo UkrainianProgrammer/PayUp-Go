bb354bf07ab04c0fd85e642010260bc7fe6e0a2b
Alex S <insten490@gmail.com>
2025-04-07 23:02:58 -0500

    running server continuously

--- back-end/src/main.go
+++ back-end/src/main.go
@@ -10,7 +10,6 @@ import (
 
 func main() {
 	http.HandleFunc("/create-payment-intent", handleCreatePaymentIntent)
-	// http.HandleFunc("health")
 }
 
 func handleCreatePaymentIntent(w http.ResponseWriter, r *http.Request) {
