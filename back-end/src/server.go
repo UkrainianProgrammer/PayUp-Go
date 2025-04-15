fcde829fac31d06f2a91e3c02d1bd67630501b0a
Alex S <insten490@gmail.com>
2025-04-14 22:50:28 -0500

    renamed to server

---
+++ back-end/src/server.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// http://localhost:4242/create-initial-payment
+
+func main() {
+	http.HandleFunc("/create-payment-intent", handleCreatePaymentIntent)
+
+	log.Println("Listening on port 4242")
+	var err error = http.ListenAndServe("localhost:4242", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handleCreatePaymentIntent(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint called!")
+}
