a4794efcdfd8bc19bddfe730ad1d3a1bddd8d55f
Alex S <insten490@gmail.com>
2025-04-24 21:50:08 -0500

    added request struct

--- back-end/src/server.go
+++ back-end/src/server.go
@@ -25,7 +25,17 @@ func handleCreatePaymentIntent(writer http.ResponseWriter, request *http.Request
 		return
 	}
 
-	fmt.Println("request method was correct!")
+	var req struct {
+		ProductId string `json:"product_id"`
+		FirstName string `json:"first_name"`
+		LastName  string `json:"last_name"`
+		Address1  string `json:"address_1"`
+		Address2  string `json:"address_2"`
+		City      string `json:"city"`
+		State     string `json:"state"`
+		Zip       string `json:"zip"`
+		Country   string `json:"country"`
+	}
 }
 
 func handleHealth(writer http.ResponseWriter, request *http.Request) {
